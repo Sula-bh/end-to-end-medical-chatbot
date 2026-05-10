bbf5cfb45294de21c58acd11be18a4bd91253100
Sulabh Acharya <sulava06@gmail.com>
2026-05-11 01:27:32 +0545

    remove filename parameter from logging configuration for improved handler setup

--- src/med_chatbot/logger.py
+++ src/med_chatbot/logger.py
@@ -12,7 +12,6 @@ LOG_FILEPATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=LOG_FILEPATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler(LOG_FILEPATH), logging.StreamHandler()],
 )
