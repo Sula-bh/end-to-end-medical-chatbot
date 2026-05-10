efe79566e9694b0d2b40713b8fd6d2322c15d1f2
Sulabh Acharya <sulava06@gmail.com>
2026-05-10 22:20:51 +0545

    initialize setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="med-chatbot",
+    version="0.0.1",
+    author="Sulabh Acharya",
+    author_email="sulava06@gmail.com",
+    packages=find_packages(),
+)
