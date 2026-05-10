801ba35c6458002c6efb93abfc100d10a688598b
Sulabh Acharya <sulava06@gmail.com>
2026-05-10 20:13:16 +0545

    add folder structure

---
+++ project_setup.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
+
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    "src/logger.py",
+    ".env",
+    "setup.py",
+    "notebooks/experiment.ipynb",
+    "app.py",
+    "store_index.py",
+    "data/.gitkeep",
+    "static/.gitkeep",
+    "templates/chat.html",
+    "requirements.txt",
+    "README.md",
+    ".gitignore",
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "" and not os.path.exists(filedir):
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file {filename}")
+
+    if not os.path.exists(filepath):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already created")
